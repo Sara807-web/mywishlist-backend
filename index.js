89d8561a7bd2e506da40999fb5ef268eb2f84197
Sara <Sara.Rezaii@Student.HTW-Berlin.de>
2026-08-24 18:34:45 +0200

    add API welcome endpoint

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the MyWishlist API");
+});
 app.listen(port, () => {
   console.log(`MyWishlist backend is running on port ${port}`);
 });
