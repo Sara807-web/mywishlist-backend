4e83f2307295b1b75adec85f97e6256d0d80cd6a
Sara <Sara.Rezaii@Student.HTW-Berlin.de>
2026-08-24 18:55:39 +0200

    add get wishes endpoint

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Welcome to the MyWishlist API");
 });
+app.get("/wishes", (req, res) => {
+  res.json([]);
+});
 app.listen(port, () => {
   console.log(`MyWishlist backend is running on port ${port}`);
 });
