d6c87138c2d879d61274abb7ea6c15f9b5ce6e0c
Sara <Sara.Rezaii@Student.HTW-Berlin.de>
2026-09-13 16:00:03 +0200

    add database seed data

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO wishes (name, price, bought, priority)
+VALUES
+  ('Reise nach Italien', 800.00, FALSE, 'high'),
+  ('Konzertticket', 75.00, FALSE, 'high'),
+  ('Kochbuch', 24.99, FALSE, 'low');
+  
